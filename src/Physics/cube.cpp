beee12d930a380c23feb80e9fff336dd1c0f975e
GalaxyShard <swift.galaxyshard@gmail.com>
2022-01-16 01:37:29 -0500

    Collision fixes

--- src/Physics/cube.cpp
+++ src/Physics/cube.cpp
@@ -6,30 +6,45 @@ bool within(Vector3 a, Vector3 min, Vector3 max)
         && Math::within(a.y, min.y, max.y)
         && Math::within(a.z, min.z, max.z);
 }
-// doesnt work for some reason, seems to not apply rotation at all
+// inefficient, sometimes fails at specific angles
+// todo: use seperating axis test
 bool cube0_collides_with(CubeCollider *cube0, CubeCollider *cube1)
 {
     Matrix4x4 inverseRot = Matrix4x4::translate(cube0->pos)
         * Matrix4x4::rotate(cube0->rotation).transpose()
         * Matrix4x4::translate(-cube0->pos);
+    Matrix3x3 rot = Matrix3x3::rotate(cube1->rotation);
+    //Matrix4x4 inverseRot = Matrix4x4::identity();
 
     // rotate second around the first cube so that one cube is axis-aligned, then bounds check each point
     
     Vector3 thisRadius = cube0->scale*0.5f;
-    Vector3 radius = cube1->scale*0.5f;
+    Vector3 radius = (cube1->scale*0.5f);
 
     Vector3 min = cube0->pos - thisRadius;
     Vector3 max = cube0->pos + thisRadius;
 
-    Vector3 leftDownClose = inverseRot*(cube1->pos + radius*Vector3(-1,-1,1));
-    Vector3 rightDownClose = inverseRot*(cube1->pos + radius*Vector3(1,-1,1));
-    Vector3 leftUpClose = inverseRot*(cube1->pos + radius*Vector3(-1,1,1));
-    Vector3 rightUpClose = inverseRot*(cube1->pos + radius*Vector3(1,1,1));
+    auto process = [&](float x, float y, float z)
+    { return (Vector3)(inverseRot*(cube1->pos + rot*(radius*Vector3(x,y,z)))); };
 
-    Vector3 leftDownFar = inverseRot*(cube1->pos + radius*Vector3(-1,-1,-1));
-    Vector3 rightDownFar = inverseRot*(cube1->pos + radius*Vector3(1,-1,-1));
-    Vector3 leftUpFar = inverseRot*(cube1->pos + radius*Vector3(-1,1,-1));
-    Vector3 rightUpFar = inverseRot*(cube1->pos + radius*Vector3(1,1,-1));
+    Vector3 leftDownClose = process(-1,-1,1);
+    Vector3 rightDownClose = process(1,-1,1);
+    Vector3 leftUpClose = process(-1,1,1);
+    Vector3 rightUpClose = process(1,1,1);
+
+    Vector3 leftDownFar = process(-1,-1,-1);
+    Vector3 rightDownFar = process(1,-1,-1);
+    Vector3 leftUpFar = process(-1,1,-1);
+    Vector3 rightUpFar = process(1,1,-1);
+    //Vector3 leftDownClose = inverseRot*(cube1->pos + rot*(radius*Vector3(-1,-1,1)));
+    //Vector3 rightDownClose = inverseRot*(cube1->pos + rot*(radius*Vector3(1,-1,1)));
+    //Vector3 leftUpClose = inverseRot*(cube1->pos + rot*(radius*Vector3(-1,1,1)));
+    //Vector3 rightUpClose = inverseRot*(cube1->pos + rot*(radius*Vector3(1,1,1)));
+
+    //Vector3 leftDownFar = inverseRot*(cube1->pos + rot*(radius*Vector3(-1,-1,-1)));
+    //Vector3 rightDownFar = inverseRot*(cube1->pos + rot*(radius*Vector3(1,-1,-1)));
+    //Vector3 leftUpFar = inverseRot*(cube1->pos + rot*(radius*Vector3(-1,1,-1)));
+    //Vector3 rightUpFar = inverseRot*(cube1->pos + rot*(radius*Vector3(1,1,-1)));
     
     if (within(leftDownClose, min, max)
         || within(rightDownClose, min, max)
